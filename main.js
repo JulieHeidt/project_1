0b57b56e5e8b3c02a15f7cef4a270b8500460a67
Julie Heidt <julie.a.heidt@gmail.com>
2015-08-24 20:34:53 -0700

    upload initial project outline-css and html

---
+++ main.js
@@ -0,0 +1,22 @@
+// var elem = document.getElementById("ID")
+// var 
+
+// document.getElementById("floor").onkeydown = function(){
+
+
+// 	if (e.addEventListener) e.addEventListener(eventName, handler, false);
+// 	else e.attachEvent('on' + eventName, handler);
+
+// el.addEventListener("click", function(){ 
+
+// var el = document.querySelectorAll('#div-1')[0] 
+
+
+// document.onkeydown = function() {
+
+
+// function checkForWinner(){
+
+document.getElementById("start").addEventListener("click", function(){
+	document.getElementById("left")
+});
