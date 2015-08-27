44f19eed6405776c2f04ea15de97c6098a015855
Julie Heidt <julie.a.heidt@gmail.com>
2015-08-27 15:18:42 -0700

    back engineer

--- main.js
+++ main.js
@@ -1,33 +1,56 @@
-// var score = 
-var playerOne = 0;
-// var playerTwo = 0;
+// var player2;
+// var player1; 
+// v
+var square = $("square");
+var Dot = function(){
+	this.element= $("<div class='dot'></div>");
+}
 
 $("#begin").click(function(){
-	console.log("hello");
+	setInterval(function(){
+		var dot = new Dot();
+		$(".floor").append( dot.element );
+		setTimeout( function () {
+			dot.element.addClass("move");
+		}, 100 );
+	},1000)
 });
 
-
-document.addEventListener("keydown", function(event){
+$("body").on("keydown", function(event){
 	key = event.which;
 	if(key===65 || key===83 || key===68 || key===74 || key===75 || key===76){
-	console.log("hit");	
-	checkCollision();
+		console.log("hit");	
+		checkCollision(key);
 	}
 	gameOn(key);
 	//calling this function inside allows me to access key variable in next function..
 });
 
+
+
+// add event listener for elements to end animation (Ask andre for this one) to 'destroy' dots already off viewport
+$(".dot").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+	alert("this works!")
+});
+
 //if statement to check if position of dot within the position of "fixed" square
-function checkCollision(){
-	var dotPosition = document.getElementById("dotOne").getBoundingClientRect().top
-	if(dotPosition >= 500 && dotPosition <= 600){
-	console.log("add a point");
-	playerOne++
-	}
-	else {
-	playerOne--
-	}
-}
+// function checkCollision( key ){
+
+
+// }​
+	
+	// Look to see if ANY .dot is in 'dotPosition'[key]
+	// 	//loop through all dots on screen
+		//add point	if collision is detected
+
+
+	// var dotPosition = document.getElementById("dotOne").getBoundingClientRect().top
+	// if(dotPosition >= 500 && dotPosition <= 600){
+	// 	console.log("add a point");
+
+	// }
+	// console.log(dotPosition);
+// };
 
 
 //Assigning keys in a switch statement
@@ -54,42 +77,45 @@ function gameOn(key){
 	}
 }
 
-var Dot = function(){
-	this.element="<div class='dot' id='dotOne'></div>"
+
+
+
+function createTile( loc ){
+	$(".floor").append("<div class='square start'></div>");
+	$("#"+loc).addClass(loc);
+	console.log( loc + " tile")
 }
-var dot = new Dot();
-function fallingDots(){
-	$(".floor").append(dot.element);
-}
-fallingDots();
+
+createTile("left");
+createTile("center");
+createTile("right");
 
 
-$("#begin").on("click", function(){
-	for (var i = 0; i < 30; i++) {	
-		setInterval(function(){	
-			
-			$(".dot").addClass("move"); //don't need to add (.)knows its already a class
-		}, 500);
-	};	
-});
+
+// write a function on keydown that checks the top of each object to see if they are the same
 
 
-var ballfall = document.getElementsByClassName("dot")[0];
-console.log(ballfall.getBoundingClientRect())
-console.log("hi")
 
-document.getElementById("left").addEventListener("keyDown", goalPiece);
-	function goalPiece(){
-	console.log("kjf");
-	}
-document.getElementById("center").addEventListener("keyDown", goalPiece);
-	function goalPiece(){
-	console.log("kjf");
-	}
-document.getElementById("right").addEventListener("keyDown", goalPiece);
-	function goalPiece(){
-	console.log("kjf");
-	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("center").addEventListener("keyDown", goalPiece);
+// document.getElementById("right").addEventListener("keyDown", goalPiece);
+
+// function goalPiece(){
+// 	console.log("kjf");
+// }
+
 
 
 
@@ -116,30 +142,3 @@ document.getElementById("right").addEventListener("keyDown", goalPiece);
 //     }
 // });
 // //end full screen mode
-
-function createLeftTile(){
-	$(".floor").append("<div class='square start'></div>");
-	$("#left").addClass("left");
-	console.log("sucks")
-}
-createLeftTile();
-
-function createCenterTile(){
-	$(".floor").append("<div class='square start'></div>");
-	$("#center").addClass("center");
-}
-createCenterTile();
-
-function createRightTile(){
-	$(".floor").append("<div class='square start'></div>");
-	$("#right").addClass("right");
-}
-createRightTile();
-
-
-// write a function on keydown that checks the top of each object to see if they are the same
-
-
-
-
-
