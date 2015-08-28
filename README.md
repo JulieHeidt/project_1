807ebdf5dc2f1aad343de91d4568765a93eb408e
Julie Heidt <julie.a.heidt@gmail.com>
2015-08-27 18:46:32 -0700

    I'm broke

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # project_1
+
+Each user gets the same song and 30 seconds to play. Get points for accurate hits and docked points for hits off beat.
+
+1. 3 keys - asd & jkl  -- add styles with different colors with event listener to bold 
+2. key down on beat--> 
+3. 2 players
+4. array of multiple dots? position of dots--> collision
+5. collision function--dots & square: when dot and square are at same point when keydown-->add point-->pop into an array??
+4. add health
+5. add event listener for key down--Key is clicked at time of collision
+5. start music on click have key down arguments at certain points in the music at time elapsed at time of game.
+6. Add function to see if key down happened at the right time...
+7. Points for correct key down, negative point for wrong hit. ?? 
+8. Tracking points? pop points into an array?
+9. Set interval function--iterates certain amount of time during the animation.
+6. function reset
--- main.js
+++ main.js
@@ -1,116 +1,199 @@
-// var player2;
-// var player1; 
-// v
-var square = $("square");
+var scores = {
+	playerOne: 0,
+	playerTwo: 0,
+};
+
+// var Blocks = //square
+var currPlayer = "player 1";
 var Dot = function(){
 	this.element= $("<div class='dot'></div>");
+	//var rand = Math.random();
+	var dotArray = [65,83,68];
+	var loc = dotArray[rando()];
+	$(".dot").addClass("loc"+loc);
 }
+
+var arrayOfDots = [];
+var $floor = $(".floor");
 
 $("#begin").click(function(){
 	setInterval(function(){
 		var dot = new Dot();
-		$(".floor").append( dot.element );
+		$floor.append( dot.element );
+		console.log(dot.element)
 		setTimeout( function () {
-			dot.element.addClass("move");
+			(dot.element).addClass("move");
 		}, 100 );
-	},1000)
+	},3000)
+
 });
 
-$("body").on("keydown", function(event){
-	key = event.which;
-	if(key===65 || key===83 || key===68 || key===74 || key===75 || key===76){
-		console.log("hit");	
-		checkCollision(key);
+function rando() {
+	return Math.floor( Math.random() * 3 );
+}
+
+function playerCheck(){
+	if (player1 === true) {
+		console.log("Player 1")
+	}else {
+		console.log("Player 1 is false")
+		player1 = true;
+		console.log("Player was set to " + playerOne);
 	}
-	gameOn(key);
-	//calling this function inside allows me to access key variable in next function..
-});
+}
 
+function keyDown(e){
+	var Blocks = $(".block.loc" + e.which);
+	for (var i = 0; i < block.length; i++) {
+		if(blocks[i].offset().top>=500  && blocks[i].offset().top<=600){
+		scores[currPlayer]++;
+		}
+		console.log("works");
+	};
+}
 
+function createTile( loc ){
+	//$(".floor").append("<div class='square start'></div>");
+	//$("#"+loc).addClass(loc);
+	console.log( loc + " tile");
+	createTile("left");
+	console.log("left")
+	createTile("center");
+	createTile("right");
+}
 
-// add event listener for elements to end animation (Ask andre for this one) to 'destroy' dots already off viewport
-$(".dot").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-	alert("this works!")
-});
+ function createLeftTile(){
+ 	$(".floor").append("<div class='square start'></div>");
+ 	$("#left").addClass("left");
+ 	console.log("sucks")
+ }
+createLeftTile();
 
-//if statement to check if position of dot within the position of "fixed" square
+ function createCenterTile(){
+ 	$(".floor").append("<div class='square start'></div>");
+ 	$("#center").addClass("center");
+ }
+createCenterTile();
 
-// function checkCollision (key){
-// 	console.log('test');
+function createRightTile(){
+ 	$(".floor").append("<div class='square start'></div>");
+ 	$("#right").addClass("right");
+}
+createRightTile();
 
-// }​
-	
-	// Look to see if ANY .dot is in 'dotPosition'[key]
-	// 	//loop through all dots on screen
-		//add point	if collision is detected
-
-
-	// var dotPosition = document.getElementById("dotOne").getBoundingClientRect().top
-	// if(dotPosition >= 500 && dotPosition <= 600){
-	// 	console.log("add a point");
-
-	// }
-	// console.log(dotPosition);
-// };
-
-
-//Assigning keys in a switch statement
 function gameOn(key){
 	switch(key){
 	case 65:  //if key equals (case) in this instance 65
 		console.log("you have clicked A");
 		break;
 	case 83:
-		console.log("you have clicked B");
+		console.log("you have clicked S");
 		break;
 	case 68:  
-		console.log("you have clicked C");
-		break;
-	case 74:  
-		console.log("you have clicked J");
-		break;
-	case 75:  
-		console.log("you have clicked K");
-		break;
-	case 76:  
-		console.log("you have clicked L");
+		console.log("you have clicked D");
 		break;
 	}
 }
 
-// <<<<<<< HEAD
+
+$("body").on("keydown", function(event){
+	key = event.which;
+	if(key===65 || key===83 || key===68) {
+		console.log("hit");	
+		}
+	});
+// function kill (){
 
 
 
-function createTile( loc ){
-	$(".floor").append("<div class='square start'></div>");
-	$("#"+loc).addClass(loc);
-	console.log( loc + " tile")
-// =======
-// var Dot = function(){
-// 	this.element="<div class='dot ball' id='dotOne'></div>"
-// 	$(".dot").addClass("move");
-// }
+document.getElementById("center").addEventListener("keyDown", goalPiece);
+document.getElementById("right").addEventListener("keyDown", goalPiece);
 
-// var Dot = function(){
-// 	this.element="<div class='dot ball' id='dotTwo'></div>"
-// 	$(".dot").addClass("move");
-// }
+function goalPiece(){
+	console.log("kjf");
+}
 
-// var Dot = function(){
-// 	this.element="<div class='dot ball' id='dotThree'></div>"
-// 	$(".dot").addClass("move");
-// }
+
+
+
+createTile();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 	gameOn(key);
+// 	//calling this function inside allows me to access key variable in next function..
+// });
+
 
 // var dot = new Dot();
 // function fallingDots(){
 // 	$(".floor").append(dot.element);
-// >>>>>>> f99a807d28b8323893c08c56274b4e7523d63a7f
-}
+// 	}
 
-createTile("left");
-createTile("center");
-createTile("right");
+
+// // add event listener for elements to end animation (Ask andre for this one) to 'destroy' dots already off viewport
+// $(".dot").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+// 	alert("this works!")
+// });
+
+// //if statement to check if position of dot within the position of "fixed" square
+
+// function checkCollision (key){
+
+// 	console.log('test');
+
+// // }​
+	
+// 	// Look to see if ANY .dot is in 'dotPosition'[key]
+// 	// 	//loop through all dots on screen
+// 		//add point	if collision is detected
+
+
+// 	// var dotPosition = document.getElementById("dotOne").getBoundingClientRect().top
+// 	// if(dotPosition >= 500 && dotPosition <= 600){
+// 	// 	console.log("add a point");
+
+// 	// }
+// 	// console.log(dotPosition);
+// // };
+
+
+// //Assigning keys in a switch statement
+// function gameOn(key){
+// 	switch(key){
+// 	case 65:  //if key equals (case) in this instance 65
+// 		console.log("you have clicked A");
+// 		break;
+// 	case 83:
+// 		console.log("you have clicked B");
+// 		break;
+// 	case 68:  
+// 		console.log("you have clicked C");
+// 		break;
+// 	case 74:  
+// 		console.log("you have clicked J");
+// 		break;
+// 	case 75:  
+// 		console.log("you have clicked K");
+// 		break;
+// 	case 76:  
+// 		console.log("you have clicked L");
+// 		break;
+// 	}
+// }
+
 
 
 
@@ -141,12 +224,6 @@ createTile("right");
 
 
 
-// document.getElementById("center").addEventListener("keyDown", goalPiece);
-// document.getElementById("right").addEventListener("keyDown", goalPiece);
-
-// function goalPiece(){
-// 	console.log("kjf");
-// }
 
 
 
